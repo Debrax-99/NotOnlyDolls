8e164f6abe611d52977a1879a7d16de333a2fe44
Ana B <am.blan.esc@gmail.com>
2021-04-05 18:32:30 +0200

    Initial commit
    Project initialization with spring and mvn

---
+++ src/main/java/com/notonlydolls/nodwebsite/NodwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.notonlydolls.nodwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notonlydolls/nodwebsite/NodwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notonlydolls.nodwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
