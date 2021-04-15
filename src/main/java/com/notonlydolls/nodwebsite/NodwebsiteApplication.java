803556cf468e79fd4258fe396acc58ad8e57198d
Ana B <am.blan.esc@gmail.com>
2021-04-15 22:48:50 +0200

    Fifth Commit
    CRUD Services for blog posts created and tested in application with CommandLineRunner

--- src/main/java/com/notonlydolls/nodwebsite/NodwebsiteApplication.java
+++ src/main/java/com/notonlydolls/nodwebsite/NodwebsiteApplication.java
@@ -1,15 +1,82 @@
 package com.notonlydolls.nodwebsite;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.notonlydolls.nodwebsite.repository.BlogPost;
+import com.notonlydolls.nodwebsite.services.BlogPostServiceI;
+
+/**
+ * Clase "Aplicación Spring Boot" que consume servicios de gestión de Backoffice
+ * 
+ * @author Ana Blanco Escudero
+ * @since 06-04-21
+ */
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.notonlydolls.repository")
-public class NodwebsiteApplication {
+@EnableMongoRepositories(basePackages = "com.notonlydolls.nodwebsite.repository")
+public class NodwebsiteApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NodwebsiteApplication.class, args);
+	}
+
+	/** Servicio de consultas y gestión de noticias del blog */
+	@Autowired
+	private BlogPostServiceI blogPostService;
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		// Creación de 3 noticias e inserción de las mismas en la base de datos
+		System.out.println("****************** INSERT BLOG POSTS ******************");
+		BlogPost blogPost1 = new BlogPost();
+		blogPost1.setPublished(false);
+		blogPost1.setTitle("Hello World 1 from NoD!");
+		blogPost1.setCategory("Curiosidades");
+		blogPost1.setAuthor("Debrax");
+		blogPost1.setText("Esto es un texto de prueba 1.");
+		blogPostService.insert(blogPost1);
+
+		BlogPost blogPost2 = new BlogPost();
+		blogPost2.setPublished(false);
+		blogPost2.setTitle("Hello World 2 from NoD!");
+		blogPost2.setCategory("Curiosidades");
+		blogPost2.setAuthor("Debrax");
+		blogPost2.setText("Esto es un texto de prueba 2.");
+		blogPostService.insert(blogPost2);
+
+		BlogPost blogPost3 = new BlogPost();
+		blogPost3.setPublished(false);
+		blogPost3.setTitle("Hello World 3 from NoD!");
+		blogPost3.setCategory("Curiosidades");
+		blogPost3.setAuthor("Debrax");
+		blogPost3.setText("Esto es un texto de prueba 3.");
+		blogPostService.insert(blogPost3);
+
+		System.out.println("*****************************************************");
+
+		// Búsqueda de todas las noticias existentes
+		List<BlogPost> blogPostList = blogPostService.searchAll();
+
+		// Búsqueda de una noticia por su ID
+		blogPostService.searchById(blogPostList.get(1).getId());
+
+		// Eliminación de la noticia 2
+		blogPostService.delete(blogPostList.get(1).getId());
+
+		// Modificación del texto de la noticia 3
+		blogPost3.setText("Esto es otro texto de prueba 3.");
+		blogPostService.update(blogPost3);
+
 	}
 
 }
---
+++ src/main/java/com/notonlydolls/nodwebsite/services/BlogPostServiceI.java
@@ -0,0 +1,50 @@
+package com.notonlydolls.nodwebsite.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.notonlydolls.nodwebsite.repository.BlogPost;
+
+/**
+ * Clase "Servicio" de consultas y gestión de noticias del Blog
+ * 
+ * @author Ana Blanco Escudero
+ * @since 15-04-21
+ */
+public interface BlogPostServiceI {
+	/**
+	 * Método que inserta una noticia nueva en la base de datos
+	 * 
+	 * @param entity
+	 */
+	public void insert(final BlogPost entity);
+
+	/**
+	 * Método que consulta todos las noticias en la base de datos
+	 * 
+	 * @return List<BlogPost>
+	 */
+	public List<BlogPost> searchAll();
+
+	/**
+	 * Método que consulta una noticia en la base de datos a partir de su ID
+	 * 
+	 * @param id
+	 */
+	public Optional<BlogPost> searchById(final String id);
+
+	/**
+	 * Método que borra una noticia en la base de datos a partir de su ID
+	 * 
+	 * @param id
+	 */
+	public void delete(final String id);
+
+	/**
+	 * Método que actualiza los datos de una noticia en la base de datos
+	 * 
+	 * @param entity
+	 */
+	public void update(final BlogPost entity);
+
+}
